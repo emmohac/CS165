f2a3e1cfd5691ce65640b42378255f2bd649dafd
huymt2 <emmohac.tran@yahoo.com>
2020-06-17 22:29:53 -0700

    Update plot file

--- project3/graph/project3p2.py
+++ project3/graph/project3p2.py
@@ -2,20 +2,59 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data/er.csv")
-diameter = df["diameter"]
-size = df["size"]
+data = np.genfromtxt("data/er_100k.csv", delimiter=",")
+degree = data[1:,0]
+vertices = data[1:,1]
+n = 100000
 
-m, b = np.polyfit(np.array(size), np.array(diameter),1)
-#plt.loglog(np.array(size), diameter, 'yo', basex=2, alpha=0.6, label=f'ER diameter ~ {m}logN + {b}', color='red')
-plt.semilogx(np.array(size), np.array(diameter), 'yo', basex=2, alpha=0.6, label=f'ER diameter ~ {m}logN + {b}', color='red')
-#plt.loglog(np.array(size), 2 ** (m * np.log2(np.array(size)) + b), basex=2, basey=2, color='blue')
-#plt.loglog(np.log2(np.array(size)), 2 ** (m * np.log2(np.array(size)) + b), basex=2, color='blue')
+f, (plt1,plt2) = plt.subplots(1,2, figsize=(11,4))
+#plt1.bar(degree, vertices, width = 0.7,alpha=0.7)
+plt1.plot(degree, vertices, color='red')
+plt1.set_xlim(min(degree), max(degree))
+plt1.set_xlabel('Degree',fontsize=12)
+plt1.set_ylabel('Number of vertices',fontsize=12)
+plt1.set_title(f'Erdós-Rényi lin-lin scale for n = {n}',fontsize=12)
 
-test_array = [2**i for i in range(3, 18)]
-#plt.scatter(np.log2(np.array(size)), np.array(diameter), color="blue")
-a = [2**i for i in range(3,18)]
-#plt.xticks(a)
-#plt.semilogy((np.array(size)), np.array(diameter), color='blue')
+d = list()
+v = list()
+for i in range(len(degree)):
+    if degree[i] != 0 and vertices[i] != 0:
+        d.append(degree[i])
+        v.append(vertices[i])
+        
+m, b = np.polyfit(np.log2(np.array(d)), np.log2(np.array(v)), 1)
+
+plt2.loglog(np.array(d), v, 'yo', basex=2, basey=2, label=f'{round(m,5)}logN + {round(b,5)}', color='red')
+plt2.set_xlabel('Degree',fontsize=12)
+plt2.set_ylabel('Number of vertices',fontsize=12)
+plt2.set_title(f"Erdós-Rényi log-log scale for n = {n}")
+plt2.legend(loc="upper left")
+f.subplots_adjust(hspace=0.4, wspace=1)
+plt.tight_layout()
 plt.show()
-print("Done")
+
+df = pd.read_csv("data/er.csv")
+diameter = df['diameter']
+size = df['size']
+
+m, b = np.polyfit(np.log2(np.array(size)),np.array(diameter), 1)
+plt.figure(figsize=(8,5))
+plt.semilogx(np.array(size), np.array(diameter), 'yo', basex=2, alpha=0.6, label=f'Diameter', color='red')
+plt.plot(np.array(size), (m * np.log2(np.array(size)) + b), label=f'best fit ~ {round(m,5)}logN + {round(b,5)}', color='blue')
+plt.xlabel('Size N',fontsize=12)
+plt.ylabel('Diameter',fontsize=12)
+plt.legend()
+plt.title("Diameter of Erdós-Rényi model")
+plt.show()
+
+clus_coef = df["clus_coef"]
+
+m, b = np.polyfit(np.log2(np.array(size)),np.array(clus_coef), 1)
+plt.figure(figsize=(8,5))
+plt.semilogx(np.array(size), np.array(clus_coef), 'yo', basex=2, alpha=0.6, label=f'Clustering Coefficient', color='red')
+plt.plot(np.array(size), (m * np.log2(np.array(size)) + b), label=f'best fit ~ {round(m,5)}logN + {round(b,5)}', color='blue')
+plt.xlabel('Size N',fontsize=12)
+plt.ylabel('Clustering Coefficient',fontsize=12)
+plt.legend()
+plt.title("Clustering Coefficient of Erdós-Rényi model")
+plt.show()
